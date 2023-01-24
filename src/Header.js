2f381fcd48496f57b0df0be397594af9dc224a15
FrancescaBambozzi <bambozzif@gmail.com>
2023-01-24 16:55:52 +0100

    commit

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header-container">
+    <h1
+        className="header-title"
+      >
+        hotels booking
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
