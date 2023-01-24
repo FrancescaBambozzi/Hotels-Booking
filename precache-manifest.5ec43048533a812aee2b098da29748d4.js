18ddac9e0004f5750fd2b085b07e74a80827c943
FrancescaBambozzi <bambozzif@gmail.com>
2023-01-24 16:47:08 +0100

    Deploy React app to GitHub Pages

---
+++ precache-manifest.5ec43048533a812aee2b098da29748d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "108505cf1182a08e5a5d",
+    "url": "/Hotels-Booking/static/css/main.99bd8d9a.chunk.css"
+  },
+  {
+    "revision": "108505cf1182a08e5a5d",
+    "url": "/Hotels-Booking/static/js/main.8649c526.chunk.js"
+  },
+  {
+    "revision": "374f8e58e5e72eab7277",
+    "url": "/Hotels-Booking/static/js/runtime~main.3c89ee0c.js"
+  },
+  {
+    "revision": "6b7151bbcd55a4e67582",
+    "url": "/Hotels-Booking/static/js/2.98a88802.chunk.js"
+  },
+  {
+    "revision": "c57939335d24d598fbba5486015d9684",
+    "url": "/Hotels-Booking/static/media/body.c5793933.jpg"
+  },
+  {
+    "revision": "9f565ed896c88668cdfefd5e4874f2f2",
+    "url": "/Hotels-Booking/index.html"
+  }
+];
